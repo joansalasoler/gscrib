4044559c330a1f712c371f2973b046d81917fb7f
Joan Sala <contact@joansala.com>
2025-04-15 11:52:55 +0200

    Remove sys dependency

--- gscrib/gcode_core.py
+++ gscrib/gcode_core.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys, logging
+import logging
 from contextlib import contextmanager
 from typing import Any, List, Sequence, Tuple
 from typeguard import typechecked
